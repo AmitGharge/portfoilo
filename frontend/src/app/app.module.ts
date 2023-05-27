3d499e2fa01e1e5a1f2f57d1687819dbc995fc13
Amit Gharge <amitgharge99@gmail.com>
2023-05-27 15:06:58 +0530

    Header basic completed, other Component initialzed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HeaderComponent } from './shared/reusable-components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ContactComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
